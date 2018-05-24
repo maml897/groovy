eb94ff655193eb1e4b54abc3faf0862a358cf38f
maml <maml897@163.com>
2018-05-24 08:10:24 +0800

    ok

---
+++ src/main/java/groovy/Klh.groovy
@@ -0,0 +1,18 @@
+package groovy
+
+def test = { a,b,c -> a+b+c }      
+def testfun = test.curry(10)   
+println testfun(3, 4)       
+
+
+
+def tellFortunes(closure) {
+	Date date = new Date("09/20/2012")
+	postFortune = closure.curry(date)
+	postFortune "Your day is filled with ceremony"
+	postFortune "You are features, not bugs"
+}
+
+tellFortunes() {date,fortune ->
+	println "Fortune for ${date} is '${fortune}'"
+}
